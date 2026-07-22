a832c4e166d5c9ec3d38b73956c18845acbeec4e
hungnd <hungnd@navisoft.com.vn>
2026-07-22 16:46:44 +0700

    Fix groovy file

--- Pipelines/Backend/pipeline-backend-master.groovy
+++ Pipelines/Backend/pipeline-backend-master.groovy
@@ -11,10 +11,12 @@ pipeline {
     stages {
         stage('Build Image & push image') {
             steps {
-                echo 'Build image'
-                docker.withRegistry(REGISTRY_URL, DOCKER_CREDENTIAL_ID) {
-                    def myImage = docker.build("${IMAGE_NAME}:${IMAGE_TAG}", ".")
-                    myImage.push()
+                script {
+                    echo 'Build image'
+                    docker.withRegistry(REGISTRY_URL, DOCKER_CREDENTIAL_ID) {
+                        def myImage = docker.build("${IMAGE_NAME}:${IMAGE_TAG}", ".")
+                        myImage.push()
+                    }
                 }
             }
         }
